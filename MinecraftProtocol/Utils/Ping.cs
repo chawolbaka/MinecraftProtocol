d3b82f0ae1031579cd7c7c9bfd9acdd1046a47e4
chawol <chawolbaka@hotmail.com>
2018-08-08 22:55:26 +0800

    try to support Round-robin DNS

--- MinecraftProtocol/Utils/Ping.cs
+++ MinecraftProtocol/Utils/Ping.cs
@@ -24,19 +24,71 @@ namespace MinecraftProtocol.Utils
         /// Not Support Legacy Ping(https://wiki.vg/Server_List_Ping#1.6)
         /// </summary>
         /// <param name="host">Server IP Address or Domain Name</param>
-        public Ping(string host, ushort port)
+        /// <param name="UseDnsRoundRobin">
+        /// 一种拿DNS来负载均衡的方法
+        /// 大概是这样的:如果多条同名记录的话 DNS会循环提供这些记录(全部都会提供,循环的是顺序)
+        /// 禁用这个选项的话会只使用最前面的那条记录中的IP
+        /// (不禁用的话会使用ICMP的Ping来检测所以记录中哪条的延迟最低,并使用那条记录来进行接下来的工作)
+        /// </param>
+        public Ping(string host, ushort port,bool UseDnsRoundRobin=true)
         {
             if (string.IsNullOrWhiteSpace(host))
-                throw new ArgumentNullException("Parameter \"serverIP\" is null or WhiteSpace");
+                throw new ArgumentNullException("Parameter \"host\" is null or WhiteSpace");
             if (Regex.Match(host, @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$").Success == false)//域名的正则我写不出来...(这个都是抄来的)
             {
                 IPHostEntry hostInfo = Dns.GetHostEntry(host);
-                this.ServerIPAddress = hostInfo.AddressList[0].ToString();//为什么有不止一条的记录?
+                if (UseDnsRoundRobin == true && hostInfo.AddressList.Length > 1)
+                {
+                    //一种拿DNS来负载均衡的方法,如果多条同名记录的话 DNS会循环提供这些记录的IP
+                    //但是这些IP只是位置会循环变更,并不是只能查询到一条记录,所以我这边使用ICMP的ping来查询那条记录延迟最低
+                    //我不确定这个会不会有什么严重的bug,所以现在暂时只在这边使用
+                    
+                    System.Net.NetworkInformation.PingException buff=null;
+                    long? LastTime=null;
+                    foreach (var ip in hostInfo.AddressList)
+                    {
+                        try
+                        {
+                            using (System.Net.NetworkInformation.Ping ping = new System.Net.NetworkInformation.Ping())
+                            {
+                                var pingResulr = ping.Send(this.ServerIPAddress, 1000 * 10);
+                                if (pingResulr.Status == System.Net.NetworkInformation.IPStatus.Success)
+                                {
+                                    if (LastTime == null|| pingResulr.RoundtripTime < LastTime)
+                                    {
+                                        LastTime = pingResulr.RoundtripTime;
+                                        this.ServerIPAddress = ip.ToString();
+                                    }
+                                    else
+                                        continue;
+                                }
+                            }
+                        }
+                        catch (System.Net.NetworkInformation.PingException e)
+                        {
+                            //这一条Ping不通没关系,继续Ping下一条.
+                            //(缓存起来是为了防止出现所以IP都不可用的情况,那种情况下的话遍历结束后会重新抛出异常)
+                            buff = e;
+                            continue;
+                        }
+                        finally
+                        {
+                            if (string.IsNullOrWhiteSpace(ServerIPAddress) && buff != null)
+                                throw buff;
+                            else if (string.IsNullOrWhiteSpace(ServerIPAddress) && buff == null)
+                                throw new Exception("DNS记录中没有可用的IP");
+                        }
+                    }
+                }
+                else
+                    this.ServerIPAddress = hostInfo.AddressList[0].ToString();
             }
             else
                 this.ServerIPAddress = host;
             this.ServerPort = port;
         }
+        public Ping(IPEndPoint IP, bool UseDnsRoundRobin = true) :this(IP.Address.ToString(), (ushort) IP.Port, UseDnsRoundRobin)
+        { }
         public PingReply Send()
         {
             
