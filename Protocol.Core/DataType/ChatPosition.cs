8441cbea7337256f1e7a9ca4feee83f58f5d0df5
chawol <chawolbaka@hotmail.com>
2023-02-11 23:35:21 +0800

    补充遗落的枚举

---
+++ Protocol.Core/DataType/ChatPosition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinecraftProtocol.DataType
+{
+    public enum ChatPosition : byte
+    {
+        ChatMessage = 0,
+        SystemMessage = 1,
+        GameInfo = 2
+    }
+}
